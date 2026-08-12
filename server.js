331b4a30ce708e39290f48a8a5dd4a7efdc492fa
Ameer Khalil <dr.ameerkhalil@Mac.attlocal.net>
2026-08-12 11:13:27 -0500

    fix invoice import failing on files over 100kb (Express body limit)

--- server.js
+++ server.js
@@ -397,7 +397,7 @@ app.post('/api/webhooks/stripe', express.raw({ type: 'application/json' }), (req
   res.json({ received: true });
 });
 
-app.use(express.json());
+app.use(express.json({ limit: '25mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
